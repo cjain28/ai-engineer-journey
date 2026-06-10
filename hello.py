2aae8fb36c6d5f77e8c78ac4d1b5f80cf55e62ea
er.cj.jain <er.cj.jain@gmail.com>
2026-06-10 21:05:13 +0530

    Day 1 - first Python file

---
+++ hello.py
@@ -0,0 +1,7 @@
+name = "Chirag Jain"
+languages = ["JavaScript", "React", "Python"]
+
+for lang in languages:
+    print(f"I know {lang}")
+
+print(f"My name is {name} and I am trying to become an AI Engineer")
